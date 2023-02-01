e01e200c359e8fce7571a658612e4c32da964e64
Rafferty Uy <rafferty.d.uy@outlook.com>
2023-02-01 14:00:23 +0800

    Add iframe tab to hub group

---
+++ RazType-ADO-Extensions-Hot-Reload-and-Debug/src/Hub-iFrame/Hub-iFrame.tsx
@@ -0,0 +1,25 @@
+import "azure-devops-ui/Core/override.css";
+import "./Hub-iFrame.scss";
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import * as SDK from "azure-devops-extension-sdk";
+
+import { Page } from "azure-devops-ui/Page";
+
+class HubIFrameContent extends React.Component<{}, {}> {
+
+    public componentDidMount() {
+        SDK.init();
+    }
+
+    public render(): JSX.Element {
+        return (
+            <Page className="hub-iframe flex-grow">
+                <iframe src="https://www.raffertyuy.com" frameBorder="0"></iframe>
+            </Page>
+        );
+    }
+}
+
+ReactDOM.render(<HubIFrameContent />, document.getElementById("root"));
